cb48a00f889ac527bccc604fa6bd7562ce6524f5
Nipun Kumar <nipunvirat0@gmail.com>
2022-01-05 18:09:44 +0530

    Map

---
+++ app/src/main/java/com/bit/gdsc/edu_digital/common/Constants.kt
@@ -0,0 +1,7 @@
+package com.bit.gdsc.edu_digital.common
+
+object Constants {
+    val courseMap = mapOf<Int,String>(
+        1 to "DSATopicWise" , 2 to "CPPTopicWise", 3 to "JAVATopicWise"
+    )
+}
